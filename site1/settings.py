a183d21227d17845f043f1745d92505c9bf34ea6
audgks5551 <audgks5551@daum.net>
2021-12-06 18:53:41 +0900

    db를 mysql로 변경 및 migrate 설치

--- site1/settings.py
+++ site1/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'site1', # DB명
+        'USER': 'root', # DBMS 접속 아이디
+        'PASSWORD': '1234', # DBMS 접속 비번
+        'HOST': '127.0.0.1', # DBMS 주소
+        'PORT': '3306', # DBMS 포트
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
